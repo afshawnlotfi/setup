6e7a941d0ef4df9789351d365a1bcc3e3ce2a24e
Afshawn Lotfi <me@afshawn.org>
2021-10-05 14:09:41 -0700

    Update installation.sh

--- mac/installation.sh
+++ mac/installation.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-bash ../shared_unix/installation.sh &&
+bash ../shared_unix/installation.sh
 
 #Downloading and Installing Homebrew Suite
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 #Downloading and installing necessary desktop apps
-brew install --cask visual-studio-code firefox protonvpn postman docker ultimaker-cura spotify
+brew install --cask visual-studio-code firefox protonvpn docker ultimaker-cura spotify vmware-fusion
 
 #Downloading and installing necessary programs
 brew install fish
-
-mkdir ~/.npm-global
-npm config set prefix '~/.npm-global'
-fish -c "set -U fish_user_paths ~/.npm-global/bin $fish_user_paths"
-
-
-yarn global add eslint ts-node
 
 #Setting up mac Settings
 bash ../private/mac/installation.sh
